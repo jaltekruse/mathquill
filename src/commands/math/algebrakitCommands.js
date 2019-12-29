ae0ee309d9058b7c871c89b24b69cacbe5a1b3f4
Martijn Slob <martijn.slob@algebrakit.nl>
2019-12-29 09:48:56 +0100

    add verb command

--- src/commands/math/algebrakitCommands.js
+++ src/commands/math/algebrakitCommands.js
@@ -239,3 +239,177 @@ LatexCmds.not = P(VanillaSymbol, function (_, super_) {
     .or(optWhitespace.then(succeed(this)));
   };
 });
+
+LatexCmds.verb = P(MathCommand, function(_, super_) {
+  _.ctrlSeq = '\\verb';
+  // _.init = function(ctrlSeq, html, text) {
+  //   if (!text) text = 'text'
+  //   super_.init.call(this, ctrlSeq, html, [ text ]);
+  // };
+
+  _.html = function() {
+    return (
+        '<span class="mq-text-mode mq-akit-verb" mathquill-command-id='+this.id+'>'
+      +   this.textContents()
+      + '</span>'
+    );
+    }
+
+  _.latex = function(){ 
+    return '\\verb|'+this.textContents().trim()+'|';
+  };
+  _.text = function(){ return "'"+this.textContents()+"'" };
+  
+  _.parser = function() { 
+      var textBlock = this;
+
+      // TODO: correctly parse text mode
+      var string = Parser.string;
+      var regex = Parser.regex;
+      var optWhitespace = Parser.optWhitespace;
+      return optWhitespace
+        .then(string('|')).then(regex(/^[^\|]*/)).skip(string('|'))
+        .map(function(text) {
+          if (text.length === 0) return Fragment();
+
+          TextPiece(text).adopt(textBlock, 0, 0);
+          return textBlock;
+        });
+    };
+
+    _.createLeftOf = function(cursor) {
+      var textBlock = this;
+      super_.createLeftOf.call(this, cursor);
+    
+      if (textBlock[R].siblingCreated) textBlock[R].siblingCreated(cursor.options, L);
+      if (textBlock[L].siblingCreated) textBlock[L].siblingCreated(cursor.options, R);
+      textBlock.bubble('reflow');
+    
+      cursor.insAtRightEnd(textBlock);
+    
+      if (textBlock.replacedText)
+        for (var i = 0; i < textBlock.replacedText.length; i += 1)
+          textBlock.write(cursor, textBlock.replacedText.charAt(i));
+    };
+    
+    
+  _.numBlocks = function() { return 1; };
+
+  _.write = function(cursor, ch) {
+    if(!ch.match(/\w/)) return;
+    
+    cursor.show().deleteSelection();
+
+    if (!cursor[L]) TextPiece(ch).createLeftOf(cursor);
+    else cursor[L].appendText(ch);
+
+    this.bubble('reflow');
+  };
+
+  _.seek = function(pageX, cursor) {
+    cursor.hide();
+    var textPc = fuseChildren(this);
+
+    // insert cursor at approx position in DOMTextNode
+    var avgChWidth = this.jQ.width()/this.textContents().length;
+    var approxPosition = Math.round((pageX - this.jQ.offset().left)/avgChWidth);
+    if (approxPosition <= 0) cursor.insAtLeftEnd(this);
+    else if (approxPosition >= textPc.text.length) cursor.insAtRightEnd(this);
+    else cursor.insLeftOf(textPc.splitRight(approxPosition));
+
+    // move towards mousedown (pageX)
+    var displ = pageX - cursor.show().offset().left; // displacement
+    var dir = displ && displ < 0 ? L : R;
+    var prevDispl = dir;
+    // displ * prevDispl > 0 iff displacement direction === previous direction
+    while (cursor[dir] && displ * prevDispl > 0) {
+      cursor[dir].moveTowards(dir, cursor);
+      prevDispl = displ;
+      displ = pageX - cursor.offset().left;
+    }
+    if (dir*displ < -dir*prevDispl) cursor[-dir].moveTowards(-dir, cursor);
+
+    if (!cursor.anticursor) {
+      // about to start mouse-selecting, the anticursor is gonna get put here
+      this.anticursorPosition = cursor[L] && cursor[L].text.length;
+      // ^ get it? 'cos if there's no cursor[L], it's 0... I'm a terrible person.
+    }
+    else if (cursor.anticursor.parent === this) {
+      // mouse-selecting within this TextBlock, re-insert the anticursor
+      var cursorPosition = cursor[L] && cursor[L].text.length;;
+      if (this.anticursorPosition === cursorPosition) {
+        cursor.anticursor = Point.copy(cursor);
+      }
+      else {
+        if (this.anticursorPosition < cursorPosition) {
+          var newTextPc = cursor[L].splitRight(this.anticursorPosition);
+          cursor[L] = newTextPc;
+        }
+        else {
+          var newTextPc = cursor[R].splitRight(this.anticursorPosition - cursorPosition);
+        }
+        cursor.anticursor = Point(this, newTextPc[L], newTextPc);
+      }
+    }
+  };
+
+  // editability methods: called by the cursor for editing, cursor movements,
+  // and selection of the MathQuill tree, these all take in a direction and
+  // the cursor
+  _.moveTowards = function(dir, cursor) { cursor.insAtDirEnd(-dir, this); };
+  _.moveOutOf = function(dir, cursor) { cursor.insDirOf(dir, this); };
+  _.unselectInto = _.moveTowards;
+
+  // TODO: make these methods part of a shared mixin or something.
+  _.selectTowards = MathCommand.prototype.selectTowards;
+  _.deleteTowards = MathCommand.prototype.deleteTowards;
+
+  _.selectOutOf = function(dir, cursor) {
+    cursor.insDirOf(dir, this);
+  };
+  _.deleteOutOf = function(dir, cursor) {
+    // backspace and delete at ends of block don't unwrap
+    if (this.isEmpty()) cursor.insRightOf(this);
+  };
+
+  _.focus = MathBlock.prototype.focus;
+  _.blur = function(cursor) {
+    MathBlock.prototype.blur.call(this);
+    if (!cursor) return;
+    if (this.textContents() === '') {
+      this.remove();
+      if (cursor[L] === this) cursor[L] = this[L];
+      else if (cursor[R] === this) cursor[R] = this[R];
+    }
+    else fuseChildren(this);
+  };
+
+  function fuseChildren(self) {
+    self.jQ[0].normalize();
+
+    var textPcDom = self.jQ[0].firstChild;
+    if (!textPcDom) return;
+    pray('only node in TextBlock span is Text node', textPcDom.nodeType === 3);
+    // nodeType === 3 has meant a Text node since ancient times:
+    //   http://reference.sitepoint.com/javascript/Node/nodeType
+
+    var textPc = TextPiece(textPcDom.data);
+    textPc.jQadd(textPcDom);
+
+    self.children().disown();
+    return textPc.adopt(self, 0, 0);
+  }
+
+  _.jQadd = function(jQ) {
+    super_.jQadd.call(this, jQ);
+    if (this.ends[L]) this.ends[L].jQadd(this.jQ[0].firstChild);
+  };
+  
+  _.textContents = function() {
+    return this.foldChildren('', function(text, child) {
+      return text + child.text;
+    });
+  };
+
+
+})
